cdbc3ed94ce4aa993de274f912a3702298762e4a
Kexun Liu <154510748+PeterLiuSHS@users.noreply.github.com>
2026-07-12 17:31:33 +0100

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        // 先构造一个用于返回的结果
+        List<List<Integer>> result = new LinkedList<>();
+        // 再构造一个queue用来放节点
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (
+            !q.isEmpty()) { // 这里是用来限制弹出的次数，本轮弹出次数最大不得超过上一轮塞进去的次数
+
+            List<Integer> level = new LinkedList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+
+            result.add(level);
+        }
+        return result;
+    }
+}
