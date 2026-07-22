3deeb0c35482d91b63962c1da41aaa951b8ed003
Kexun Liu <154510748+PeterLiuSHS@users.noreply.github.com>
2026-07-22 16:40:35 +0100

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count =0;
+        for (int row = 0; row < grid.length; row++){
+            for (int col =0; col<grid[0].length; col++){
+                if (grid[row][col]=='1'){
+                    count++;
+                    dfs(grid, row, col, count);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int row, int col, int count){
+        if (row<0 || col<0 || row>=grid.length || col>=grid[0].length){
+            return ; // 结束了
+        }
+
+        if (grid[row][col]=='0'){
+            return ;// 也结束了，不用继续了
+        }
+
+        // 接下来就是元素等于1的情况了
+        grid[row][col] = '0'; // 我们先把这个元素给改了
+
+        dfs(grid, row+1, col, count);
+        dfs(grid, row-1, col, count);
+        dfs(grid, row, col+1, count);
+        dfs(grid, row, col-1, count);
+    }
+}
+
+
+
+
+
+
+
+
+
